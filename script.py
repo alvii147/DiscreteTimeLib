32d7cfcce03fb96f7afa4d117f7db8837cea3b25
Zahin <alvii147@gmail.com>
2021-10-13 10:50:59 -0400

    changed script

--- script.py
+++ script.py
@@ -1,12 +1,4 @@
 from DiscreteTimeLib import DiscreteTimeSignal
 
 if __name__ == '__main__':
-    # data = ((0, 1), (1, 2), (2, 3), (3, 4), (4, 5))
-    x = DiscreteTimeSignal()
-    print(x)
-    print(x.min_idx)
-    print(x.max_idx)
-    data = ((-1, -1), (0, 2), (1, 1))
-    h = DiscreteTimeSignal(data)
-    conv = x * h
-    print(conv)
+    pass
