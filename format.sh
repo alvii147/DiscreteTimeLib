8522e461e76e29d8ebb317b4c04723b6b0e8a065
Zahin <alvii147@gmail.com>
2021-10-20 06:31:11 +0600

    added format check script

---
+++ format.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# move to script directory
+cd "$(dirname "$0")"
+
+# run flake8
+flake8 DiscreteTimeLib/ --count --show-source --statistics
+
+# run black
+black --check --skip-string-normalization DiscreteTimeLib/
