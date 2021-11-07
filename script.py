c25d1a9afd474ba7872c2721cd2796a726c09126
Zahin <alvii147@gmail.com>
2021-11-07 17:14:20 +0600

    added testing for filter function

--- script.py
+++ script.py
@@ -1,36 +1,30 @@
 import numpy as np
 from DiscreteTimeLib import DiscreteTimeSignal, DiscreteTimeSystem
-from scipy.signal import residuez
 
 if __name__ == '__main__':
     x_n = [1, 4, 2, -1, 6]
-    data_x = [[n, x_n[n]] for n in range(len(x_n))]
+    data_x = [[n, x_n[n]] for n in range(-3, len(x_n) - 3)]
     x = DiscreteTimeSignal(data_x)
-    print(x.values())
+    print(x)
     print('')
 
     b = [1]
     a = [1, -1]
     H = DiscreteTimeSystem(b, a)
     y = H.filter(x)
-    print(y.values())
+    print(y)
     print('')
 
-    Hinv = DiscreteTimeSystem(a, b)
-    x_reconstructed = Hinv.filter(y)
-    print(x_reconstructed.values())
+    y_expected = []
+    for n in range(data_x[0][0], data_x[-1][0] + 1):
+        val = 0
+        for i in range(len(b)):
+            val += b[i] * x[n - i]
 
-    # r, p, k = residuez(a, b)
-    # print(r, p, k)
-    # print('')
-    # sig = np.zeros(signal_len)
-    # for n in range(signal_len):
-    #     if n >= 0:
-    #         for i in range(min(len(r), len(p))):
-    #             sig[n] += r[i] * pow(p[i], n)
+        for i in range(1, len(a)):
+            val -= a[i] * y[n - i]
 
-    #     if n < len(k):
-    #         sig[n] += k[n]
+        val = val / a[0]
+        y_expected.append(val)
 
-    # print(sig)
-    # print(np.convolve(x_n, sig))
+    print(y_expected)
--- tests/test_systems.py
+++ tests/test_systems.py
@@ -1,13 +1,12 @@
 import pytest
 import numpy as np
-import numpy.testing as npt
-from DiscreteTimeLib import DiscreteTimeSignal, DiscreteTimeSystem
+from DiscreteTimeLib import DiscreteTimeSystem
 
 from .utils import generate_random_system, generate_random_dts
 
 def test_DiscreteTimeSystem_init():
     b, a = generate_random_system()
-    H = DiscreteTimeSystem(b, a)
+    DiscreteTimeSystem(b, a)
 
 def test_DiscreteTimeSystem_init_error_shape():
     b, a = generate_random_system()
@@ -25,3 +24,23 @@ def test_DiscreteTimeSystem_init_error_shape():
     b, a = generate_random_system()
     with pytest.raises(ValueError):
         DiscreteTimeSystem(b, [[1, 2], [3, 4]])
+
+@pytest.mark.parametrize('execution_id', range(10))
+def test_DiscreteTimeSystem_filter(execution_id):
+    b, a = generate_random_system()
+    x_n, data_x = generate_random_dts()
+
+    H = DiscreteTimeSystem(b, a)
+    y_n = H.filter(x_n)
+
+    for n in range(y_n.min_idx, y_n.max_idx + 1):
+        y_expected = 0
+        for i in range(np.shape(b)[0]):
+            y_expected += b[i] * x_n[n - i]
+
+        for i in range(1, np.shape(a)[0]):
+            y_expected -= a[i] * y_n[n - i]
+
+        y_expected = y_expected / a[0]
+
+        assert np.allclose(y_n[n], y_expected)
