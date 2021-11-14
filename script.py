99ba4cc23e0d35d124c71eb4a8da5762f642d70e
Zahin <alvii147@gmail.com>
2021-11-14 15:14:39 +0600

    rebuilt docs

--- script.py
+++ script.py
@@ -1,6 +1,10 @@
 from DiscreteTimeLib import DiscreteTimeSignal, DiscreteTimeSystem
 
 if __name__ == '__main__':
-    H = DiscreteTimeSystem([1, 2, 1], [1, -3, 2])
-    print(H.iztrans())
-    print(H.iztrans(n_range=(0, 4)))
+    # H = DiscreteTimeSystem([1, 2, 1], [1, -3, 2])
+    # print(H.iztrans())
+    # print(H.iztrans(n_range=(0, 4)))
+    # H = DiscreteTimeSystem([1, -2, 1], [1, 0, -1])
+    A = DiscreteTimeSystem([1, -1], [1, 1])
+    B = DiscreteTimeSystem([1], [1, 1])
+    C = A * B
