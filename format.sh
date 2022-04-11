10d681715e6aff66f64957d148d26730341b522c
Zahin Zaman <alvii147@gmail.com>
2022-04-11 18:01:07 -0400

    remove requirements.txt files & testing bash scripts

--- format.sh
+++
@@ -1,8 +0,0 @@
-# move to script directory
-cd "$(dirname "$0")"
-
-# run flake8
-flake8 DiscreteTimeLib/ --count --show-source --statistics
-
-# run black
-black --check --skip-string-normalization DiscreteTimeLib/
--- test.sh
+++
@@ -1,8 +0,0 @@
-# move to script directory
-cd "$(dirname "$0")"
-
-# set PYTHONPATH
-export PYTHONPATH=$PWD
-
-# run all tests
-pytest -v -s tests/
